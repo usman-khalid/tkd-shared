bc40552846cda2a1071050dbd62da116dd66e315
Usman Khalid <usman.khalid@live.ca>
2024-10-29 10:02:16 -0400

    Initial commit

---
+++ transform.mjs
@@ -0,0 +1,27 @@
+import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+
+register(StyleDictionary, {
+  excludeParentKeys: true,
+});
+
+const sd = new StyleDictionary({
+  source: ['./tokens/**/*.json'],
+  preprocessors: ['tokens-studio'],
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio',
+      transforms: ["attribute/cti", "name/kebab", "time/seconds", "size/rem", "color/css"],
+      buildPath: 'styles/css/',
+      files: [
+        {
+          destination: 'toro.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+});
+
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
